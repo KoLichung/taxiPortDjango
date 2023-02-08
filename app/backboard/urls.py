6b9ed905a02e843becfc22c3ba3c25ed547999c3
JasonKo <jason@kosbrother.com>
2023-02-08 10:43:05 +0800

    fix

--- app/backboard/urls.py
+++ app/backboard/urls.py
@@ -4,13 +4,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 urlpatterns = [
-   
-    path('login', views.login, name='login'),
+    path('', views.login, name='login'),
+    path('logout', views.logout, name='backboard_logout'),
     path('orders', views.orders, name = 'orders'), 
     path('order_detail', views.order_detail, name = 'order_detail'), 
     path('members', views.members, name = 'members'), 
     path('member_detail', views.member_detail, name = 'member_detail'), 
     path('order_assign_driver', views.order_assign_driver, name = 'order_assign_driver'), 
     path('order_driver_status', views.order_driver_status, name = 'order_driver_status'), 
-   
 ]
--- app/backboard/views.py
+++ app/backboard/views.py
@@ -1,25 +1,69 @@
+import email
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib import auth
+from django.contrib.auth import authenticate
 
-# Create your views here.
+from modelCore.models import Case
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/backboard/orders')
+        else:
+            return redirect('/backboard/')
+
     return render(request, 'backboard/login.html')
+    
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/backboard/')
 
 def orders(request):
-    return render(request,'backboard/orders.html')
+    if not request.user.is_authenticated or not request.user.is_staff:
+        return redirect('/backboard/')
+
+    # 1.取得所需資料(並整理)
+    # 2.把所需資料傳到 template 裡面
+
+    cases = Case.objects.all().order_by('-id')
+
+    return render(request,'backboard/orders.html',{'cases':cases})
 
 def order_detail(request):
-    return render(request,'backboard/order_detail.html')
+    if not request.user.is_authenticated or not request.user.is_staff:
+        return redirect('/backboard/')
+
+    case_id = request.GET.get('case_id')
+    case = Case.objects.get(id=case_id)
+
+    return render(request,'backboard/order_detail.html',{'case':case})
 
 def members(request):
+    if not request.user.is_authenticated or not request.user.is_staff:
+        return redirect('/backboard/')
+
     return render(request,'backboard/members.html')
 
 def member_detail(request):
+    if not request.user.is_authenticated or not request.user.is_staff:
+        return redirect('/backboard/')
+
     return render(request,'backboard/member_detail.html')
 
 def order_assign_driver(request):
+    if not request.user.is_authenticated or not request.user.is_staff:
+        return redirect('/backboard/')
+
     return render(request,'backboard/order_assign_driver.html')
 
 def order_driver_status(request):
+    if not request.user.is_authenticated or not request.user.is_staff:
+        return redirect('/backboard/')
+
     return render(request,'backboard/order_driver_status.html')
