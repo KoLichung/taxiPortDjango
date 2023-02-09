7bae615140083a01b90ed0c5f0d52c477d288bf8
WenJenChun <daydreamsss@hotmail.com>
2023-02-09 18:27:11 +0800

    commit

--- app/backboard/views.py
+++ app/backboard/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib import auth
 from django.contrib.auth import authenticate
-import urllib
+import urllib, datetime
 
 from modelCore.models import Case, User
 
@@ -32,8 +32,24 @@ def orders(request):
     # 2.把所需資料傳到 template 裡面
 
     cases = Case.objects.all().order_by('-id')
+    case_status = request.POST.get('button_submit')
+
+    if case_status == 'all':
+        cases = Case.objects.all().order_by('-id')
+    elif case_status == 'wait':
+        cases = Case.objects.all().filter(case_state = 'wait').order_by('-id')
+    elif case_status == 'way_to_catch':
+        cases = Case.objects.all().filter(case_state = 'way_to_catch').order_by('-id')
+    if case_status == 'catched':
+        cases = Case.objects.all().filter(case_state = 'catched').order_by('-id').order_by('-id')
+    elif case_status == 'finished':
+        cases = Case.objects.all().filter(case_state = 'finished').order_by('-id')
+    elif case_status == 'canceled':
+        cases = Case.objects.all().filter(case_state = 'canceled').order_by('-id')
 
     return render(request,'backboard/orders.html',{'cases':cases})
+
+  
 
 def order_detail(request):
     if not request.user.is_authenticated or not request.user.is_staff:
@@ -43,6 +59,7 @@ def order_detail(request):
     case = Case.objects.get(id=case_id)
 
     return render(request,'backboard/order_detail.html',{'case':case})
+
 
 def members(request):
     if not request.user.is_authenticated or not request.user.is_staff:
@@ -59,7 +76,7 @@ def member_detail(request):
     user_id = request.GET.get('user_id')
     user = User.objects.get(id=user_id)
 
-    cases = Case.objects.filter(user=user)
+    cases = Case.objects.filter(user=user).order_by('-id')
 
 
     return render(request,'backboard/member_detail.html',{'user':user, 'cases':cases})
@@ -80,6 +97,7 @@ def order_assign_driver(request):
         case.car_model = carModel
         case.car_id_number = case_carNumberid
         case.expect_minutes = minutesToArrive
+        case.confirm_time = datetime.datetime.now()
 
         case.case_state = 'way_to_catch'
         case.save()
@@ -98,16 +116,25 @@ def order_driver_status(request):
     if request.method == 'POST':
         case_id = request.POST.get('case_id')
         case_status = request.POST.get('button_submit')
+        case_taxiFee = request.POST.get('taxiFee')
+
         case = Case.objects.get(id=case_id)
+        case.case_money = case_taxiFee
 
         # 用了 way_to_catch, catched, finished
         if case.case_state == 'way_to_catch' and case_status == 'catched':
             case.case_state = 'catched'
+            case.catched_time = datetime.datetime.now()  
         elif case.case_state == 'catched' and case_status == 'finished':
             case.case_state = 'finished'
+            case.off_time = datetime.datetime.now()
 
         case.save()
-        return redirect('order_driver_status')
+        if case.case_money != None:
+            return redirect_with_params('order_driver_status',{'case_id':case_id})
+        else :
+            return render(request,'backboard/order_driver_status.html',{'case':case})
+
 
     case_id = request.GET.get('case_id')
     case = Case.objects.get(id=case_id)
